67955917120829f69e96baea62ed834b9114d7b6
Natalia Morales <natii.vmorales98@gmail.com>
2023-11-06 17:24:53 -0300

    se modifico el ejercicio 11 para que contemple la posibilidad de ingresar 3 edades iguales

--- js/ejercicio11.js
+++ js/ejercicio11.js
@@ -1,15 +1,23 @@
 let indice = 1;
 let edadMayor = 0;
 let nombreMayor = "";
+let contador=0;
 
 while(indice <= 3){
     let edad = parseInt(prompt("Ingrese una edad: "));
     let nombre = prompt("Ingrese un nombre: ");
-    edadMayor = Math.max(edad, edadMayor);
-    if(edad === edadMayor){
-        nombreMayor = nombre;
+    if(edad===edadMayor){
+        contador++;
+    }else{
+        edadMayor = Math.max(edad, edadMayor);
+        if(edad === edadMayor){
+            nombreMayor = nombre;
+        }
     }
     indice++;
 }
-
-document.write("La persona de mayor edad es: " + nombreMayor);
+if(contador===2){
+    document.write("Las 3 edades ingresadas son iguales");
+}else{
+    document.write("La persona de mayor edad es: " + nombreMayor);
+}
